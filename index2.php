a03a559d6e8d2c887701c82561240d0bc09b09b5
wendwin <wendwin12@gmail.com>
2023-10-03 07:58:07 +0700

    membuat file index 2 dan myfunc.php (modular)

---
+++ index2.php
@@ -0,0 +1,46 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & Mysql</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <h1 class="my-5 text-center">Daftar Mahasiswa</h1>
+
+    <table  class="table table-striped" border="1" cellspacing ="0" cellpadding="10">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Gambar</th>
+                <th>NIM</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><img src="" alt=""></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <a href="">Ubah</a> |
+                    <a href="">Hapus</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
